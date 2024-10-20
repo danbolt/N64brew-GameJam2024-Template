ef860fd7eeb45958060a03729bc4766521b11a77
Daniel Savage <dansvg@gmail.com>
2024-10-20 14:46:41 -0700

    Circle model

--- code/dodge-grubby/dodge_grubby.c
+++ code/dodge-grubby/dodge_grubby.c
@@ -16,24 +16,40 @@ T3DViewport viewport;
 T3DMat4 base_model;
 T3DMat4FP base_model_fp;
 
-const T3DVec3 camera_position = { { 0, 0, -30 } };
-const T3DVec3 camera_target = { { 0, 0,   0 } };
+const T3DVec3 camera_position = { { 0, 15, -30 } };
+const T3DVec3 camera_target = { { 0, 0, 0 } };
 
-T3DVertPacked verts[2] __attribute__((aligned(16))) = {
-    (T3DVertPacked){
-        .posA = {-1, -1, 0}, .rgbaA = 0xFF0000FF,
-        .posB = { 1, -1, 0}, .rgbaB = 0x00FF00FF,
-    },
-    (T3DVertPacked){
-        .posA = { 1,  1, 0}, .rgbaA = 0x0000FFFF,
-        .posB = {-1,  1, 0}, .rgbaB = 0xFF00FFFF,
-    }
-};
+#define CIRCLE_VERT_COUNT 16
+#define CIRCLE_MODEL_RADIUS 100
+#define INV_CIRCLE_MODEL_RADIUS 0.01f
+T3DVertPacked circle_verts[CIRCLE_VERT_COUNT] __attribute__((aligned(16)));
 
 const uint8_t ambient_light[4] = {0xff, 0xff, 0xff, 0xff};
 
+void generate_circle_model()
+{
+    circle_verts[0] = (T3DVertPacked){
+        .posA = {  0,  0, 0 }, .rgbaA = 0xFFFFFFFF,
+        .posB = {  CIRCLE_MODEL_RADIUS,  0, 0 }, .rgbaB = 0xFFFFFFFF,
+    };
+
+    for (int i = 1; i < CIRCLE_VERT_COUNT; i++)
+    {
+        const float theta0 = ((float)((i * 2) + 0) / (float)(CIRCLE_VERT_COUNT * 2)) * T3D_PI * 2.0;
+        const float theta1 = ((float)((i * 2) + 1) / (float)(CIRCLE_VERT_COUNT * 2)) * T3D_PI * 2.0;
+
+        circle_verts[i] = (T3DVertPacked){
+            .posA = {  (int16_t)( cos(theta0) * CIRCLE_MODEL_RADIUS ), 0, (int16_t)( sin(theta0) * CIRCLE_MODEL_RADIUS ) }, .rgbaA = 0xFFFFFFFF,
+            .posB = {  (int16_t)( cos(theta1) * CIRCLE_MODEL_RADIUS ), 0, (int16_t)( sin(theta1) * CIRCLE_MODEL_RADIUS ) }, .rgbaB = 0xFFFFFFFF,
+        };
+    }
+    data_cache_hit_writeback(circle_verts, sizeof(circle_verts));
+}
+
 void minigame_init()
 {
+    generate_circle_model();
+
     display_init(RESOLUTION_320x240, DEPTH_16_BPP, 3, GAMMA_NONE, FILTERS_RESAMPLE);
     rdpq_init();
 
@@ -41,6 +57,8 @@ void minigame_init()
 
     viewport = t3d_viewport_create();
     t3d_mat4_identity(&base_model);
+    t3d_mat4_scale(&base_model, INV_CIRCLE_MODEL_RADIUS, INV_CIRCLE_MODEL_RADIUS, INV_CIRCLE_MODEL_RADIUS);
+    t3d_mat4_scale(&base_model, 10.f, 10.f, 10.f);
     t3d_mat4_to_fixed(&base_model_fp, &base_model);
     data_cache_hit_writeback(&base_model_fp, sizeof(T3DMat4FP));
 }
@@ -69,10 +87,12 @@ void minigame_loop(float deltatime)
     t3d_state_set_drawflags(T3D_FLAG_SHADED);
 
     t3d_matrix_push(UncachedAddr(&base_model_fp));
-    t3d_vert_load(UncachedAddr(verts), 0, 4);
+    t3d_vert_load(UncachedAddr(circle_verts), 0, CIRCLE_VERT_COUNT * 2);
     t3d_matrix_pop(1);
-    t3d_tri_draw(0, 1, 2);
-    t3d_tri_draw(2, 3, 0);
+    for (int i = 1; i < (CIRCLE_VERT_COUNT * 2) - 1; i++) {
+        t3d_tri_draw(0, i, i + 1);
+    }
+    t3d_tri_draw(0, (CIRCLE_VERT_COUNT * 2) - 1, 1);
 
     t3d_tri_sync();
 
