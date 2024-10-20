43c622681f887e651e242490ec99fbacb482c03e
Daniel Savage <dansvg@gmail.com>
2024-10-19 20:21:13 -0700

    Initial commit

---
+++ code/dodge-grubby/dodge_grubby.c
@@ -0,0 +1,30 @@
+#include <libdragon.h>
+#include "../../core.h"
+#include "../../minigame.h"
+
+const MinigameDef minigame_def = {
+    .gamename = "Dodge Grubby",
+    .developername = "Daniel Savage",
+    .description = "Avoid Grubby as he moves around the environment.",
+    .instructions = "Use the analog stick to move."
+};
+
+void minigame_init()
+{
+    //
+}
+
+void minigame_fixedloop(float deltatime)
+{
+    //
+}
+
+void minigame_loop(float deltatime)
+{
+    //
+}
+
+void minigame_cleanup()
+{
+    //
+}
---
+++ code/dodge-grubby/dodge_grubby.h
@@ -0,0 +1,9 @@
+#ifndef DODGE_GRUBBY_H
+#define DODGE_GRUBBY_H 
+
+    void dodge_grubby_init();
+    void dodge_grubby_fixedloop(float deltatime);
+    void dodge_grubby_loop(float deltatime);
+    void dodge_grubby_cleanup();
+
+#endif
